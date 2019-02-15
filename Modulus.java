4856519303f94d1c6db98685d80a39471c0cbc02
Sreedhar Palaparthy <sreedharp@gmail.com>
2019-02-15 16:38:03 +0530

    commented 2 files

--- Modulus.java
+++ Modulus.java
@@ -1,6 +1,7 @@
 Class Modulus{
 	public static double modulus(int a, int b){
 		// added comments
+		// few more comments added
 		return a/b;
 	}
 }
--- Multiply.java
+++ Multiply.java
@@ -2,6 +2,7 @@ Class Multiply{
 
 	public static long multiply(int a, int b){
 		// returns result of multiplication
+		// few more comments added
 		return a * b;
 	}
 
