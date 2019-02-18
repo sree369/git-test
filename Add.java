c772377596ad7ea561bd127d8d9cbd76a29fa49f
Sreedhar Palaparthy <sreedharp@gmail.com>
2019-02-18 15:51:40 +0530

    add comment to local repo

--- Add.java
+++ Add.java
@@ -1,7 +1,7 @@
 Class Add{
 
 	public static int add(int a, int b){
-		// add comment lines here in github repo
+		// add comment lines here in github repo. Update this locally.
 		return a + b;
 	}
 }
